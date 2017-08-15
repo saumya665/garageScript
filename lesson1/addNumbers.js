4107b753b1c52618e9fac9c3542ba9ed51a9bbf4
Saumya Singh <saummyasing@gmail.com>
2017-08-15 13:14:24 -0700

    added addNum.js

---
+++ lesson1/addNumbers.js
@@ -0,0 +1,7 @@
+const myfunc=(a,b)=>{
+  const c = a+b;
+  return c;
+}
+myfunc(2,3);
+module.exports = myfunc;
+//newest update
---
+++ lesson1/testAdd.js
@@ -0,0 +1,13 @@
+const addNumbers = require('./addNumbers.js');
+
+const test=(a,b,c)=>{
+  if(addNumbers(a,b)==c)
+  {
+    console.log("Your program is working propery");
+    return true;
+  } else {
+    console.log("Your program is not adding numbers properly");
+  }
+}
+test(3,5,8)
+test(1,1,3);
