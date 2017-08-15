6f7c5ea6ba47980fd79a1d72b7c34e692a3df4eb
saumya665 <ssingh30@ucsc.edu>
2017-08-15 14:34:53 -0700

    Update testAdd.js

--- lesson1/testAdd.js
+++ lesson1/testAdd.js
@@ -1,10 +1,9 @@
 const addNumbers = require('./addNumbers.js');
 
-const test=(a,b,c)=>{
-  if(addNumbers(a,b)==c)
-  {
-    console.log("Your program is working propery");
-  } else {
+const test=(input1,input2,total)=>{
+  if(addNumbers(input1,input2)==total){
+    console.log("Your program is working propery!")} 
+  else {
     console.log("Your program is not adding numbers properly");
   }
 }
