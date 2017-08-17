b3d9ee675c067bc303f2c4242cb9ca90d7de9e1c
Saumya Singh <saummyasing@gmail.com>
2017-08-16 19:30:20 -0700

    changed the variable names for re1 and re2

--- printNamesExpress.js
+++ printNamesExpress.js
@@ -3,14 +3,14 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 app.listen(3615);
-const myfunc = (re1,re2)=>{
+const myfunc = (req,res)=>{
   const printFunc=(err,files)=>{
     let allNames = " ";
     files.forEach((name)=>{
       if(name!="saumya")
         allNames = allNames + " " +name;
   })
-    re2.send(allNames);
+    res.send(allNames);
   };
   fs.readdir("/home", printFunc);
 };
