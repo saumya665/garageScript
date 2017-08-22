31596167a52c2b430e21868cb5a8d31a9acf989d
Saumya Singh <saumyasing@gmail.com>
2017-08-22 16:05:32 -0700

    adding

--- lesson6/lesson6.js
+++ lesson6/lesson6.js
@@ -4,7 +4,6 @@ const app = express();
 
 app.listen(3615);
 app.use(express.static('public'));
-//
 app.get('/XMLHttpRequest',(req,res)=>{
 fs.appendFile('/home/sya/garageScript/lesson6/public/textOut.txt',`/n Name: ${req.query.a1}Comment:  ${req.query.a2} \n`);
   res.send('Comment has been submitted');
