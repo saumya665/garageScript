905b8e4f0f6778fb77aebc5fbd40195dd2a18cdb
Saumya Singh <saumyasing@gmail.com>
2017-08-23 13:28:20 -0700

    fixing variable names

--- lesson5/xml.js
+++ lesson5/xml.js
@@ -6,6 +6,6 @@ app.listen(3615);
 app.use(express.static('public'));
 app.get("/XMLHttpRequest",(req,res)=>{
   fs.appendFile('/home/sya/garageScript/lesson5/public/xmlPublic.txt', `Name: ${req.query.nameBox} Comment: ${req.query.commentBox}`);
-    res.send('done');
+  res.send('done');
 });
 
