5100e1e284fdec197cbd7dc707571fd4fa41dbdc
Saumya Singh <saumyasing@gmail.com>
2017-08-25 11:32:40 -0700

    deleting files which were added from other lessons

--- lesson6/lesson6.js
+++ lesson6/lesson6.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-
 app.listen(3615);
 app.use(express.static('public'));
+
+
 app.get('/XMLHttpRequest',(req,res)=>{
-fs.appendFile('/home/sya/garageScript/lesson6/public/textOut.txt',`/n Name: ${req.query.a1}Comment:  ${req.query.a2} \n`);
+fs.writeFile('/home/sya/garageScript/lesson6/public/textOut.txt',`Name: ${req.query.nameVal} Comment:  ${req.query.comVal} \n`);
   res.send('Comment has been submitted');
 });
