631a3e5d434733c9bbd90231c8458f75623da1ec
Saumya Singh <saumyasing@gmail.com>
2017-08-28 11:58:36 -0700

    changing variable of the request

--- lesson6/lesson6.js
+++ lesson6/lesson6.js
@@ -5,7 +5,7 @@ app.listen(3615);
 app.use(express.static('public'));
 
 
-app.get('/XMLHttpRequest',(req,res)=>{
+app.get('/XMLReq',(req,res)=>{
 fs.writeFile('/home/sya/garageScript/lesson6/public/textOut.txt',`Name: ${req.query.nameVal} Comment:  ${req.query.comVal} \n`);
   res.send('Comment has been submitted');
 });
