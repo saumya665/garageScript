c55a726b3ea7ccd6c4f11fdc4dd282324b7884fb
Saumya Singh <saumyasing@gmail.com>
2017-09-26 16:17:25 -0700

    made Song's changes

--- algorithms/kabobCase/kabob.js
+++ algorithms/kabobCase/kabob.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 
 const convert=(word)=>{
-  var str = word;
-  var splitWord = str.split("");
+  let str = word;
+  let splitWord = str.split("");
   splitWord.forEach((e, index)=>{
     if(e ==e.toUpperCase()){
       e = e.toLowerCase();
       splitWord[index]="-"+e;
     }
   })
-  console.log(splitWord);
+  return(splitWord);
 }
-convert("helloMing");
+console.log(convert("helloMing"));
 
-//expected output: -hello-World
+//expected output: -hello-world
