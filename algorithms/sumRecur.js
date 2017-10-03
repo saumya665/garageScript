5716b654cf722c1e5ad10e7d4594d56a25cb5582
Saumya Singh <saumyasing@gmail.com>
2017-10-03 14:56:44 -0700

    changing the print statement

--- algorithms/sumRecur.js
+++ algorithms/sumRecur.js
@@ -1,11 +1,11 @@
 const RecurSum=(array,i=0,total=0)=>{
   if(i==array.length){
-    console.log(total);
+    return total;
   }
   else{
     total = total + array[i];
    return RecurSum(array, i+1,total);
   }
 }
-RecurSum([1,3,5,5]);
+console.log(RecurSum([1,3,5,5]));
 
